1be4652ce182241bf4e6b34fa027accf4027cb12
JLexu <juan.lexu@gmail.com>
2022-10-27 18:02:59 +0200

    fix

--- device.mk
+++ device.mk
@@ -361,9 +361,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     libhidltransport.vendor \
-    android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
+    android.hidl.base@1.0.vendor \
     libhwbinder.vendor
 
 # HotwordEnrollement app permissions
