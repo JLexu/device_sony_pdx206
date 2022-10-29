9f7a1c6c85686a2f00c37c65ea66ddc466cb3fbe
JLexu <juan.lexu@gmail.com>
2022-10-29 21:45:04 +0200

    Drop hidl base and manager from DT

--- device.mk
+++ device.mk
@@ -368,10 +368,8 @@ PRODUCT_COPY_FILES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
     libhidltransport.vendor \
     android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
     android.hidl.base@1.0.vendor \
     libhwbinder.vendor
 
